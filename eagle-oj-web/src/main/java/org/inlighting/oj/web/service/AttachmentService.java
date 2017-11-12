6d56b330e3c6aef6c7bcb7c6175e0cab647dda8a
JackGouDan <975520251@qq.com>
2017-11-12 17:30:14 +0800

    完成小组模块以及附件

--- eagle-oj-web/src/main/java/org/inlighting/oj/web/service/AttachmentService.java
+++ eagle-oj-web/src/main/java/org/inlighting/oj/web/service/AttachmentService.java
@@ -1,6 +1,9 @@
 package org.inlighting.oj.web.service;
 
 import org.apache.ibatis.session.SqlSession;
+import org.inlighting.oj.web.dao.AttachmentDao;
+import org.inlighting.oj.web.entity.AttachmentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,14 +14,25 @@ public class AttachmentService {
 
     private final SqlSession sqlSession;
 
+    private AttachmentDao attachmentDao;
+
     public AttachmentService(SqlSession sqlSession) {
         this.sqlSession = sqlSession;
     }
-
-    public int add(int owner, String url, long uploadTime) {
-        // todo
-        return 0;
+    @Autowired
+    public void setAttachmentDao(AttachmentDao attachmentDao) {
+        this.attachmentDao = attachmentDao;
     }
 
+    public int add(int owner, String url, long uploadTime) {
+        // 添加attachment
+        AttachmentEntity attachmentEntity = new AttachmentEntity();
+        attachmentEntity.setOwner(owner);
+        attachmentEntity.setUrl(url);
+        attachmentEntity.setUploadTime(uploadTime);
+        boolean flag = attachmentDao.addAttachment(sqlSession,attachmentEntity);
+
+        return flag ? attachmentEntity.getAid():0;
+    }
 
 }
--- eagle-oj-web/src/main/java/org/inlighting/oj/web/service/GroupService.java
+++ eagle-oj-web/src/main/java/org/inlighting/oj/web/service/GroupService.java
@@ -1,8 +1,10 @@
 package org.inlighting.oj.web.service;
 
 import org.apache.ibatis.session.SqlSession;
+import org.inlighting.oj.web.dao.GroupDao;
 import org.inlighting.oj.web.entity.GroupEntity;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,19 +13,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class GroupService {
 
+    private GroupDao groupDao;
+
     private final SqlSession sqlSession;
 
     public GroupService(SqlSession sqlSession) {
         this.sqlSession = sqlSession;
     }
 
+    @Autowired
+    public void setGroupDao(GroupDao groupDao) {
+        this.groupDao = groupDao;
+    }
+
     public int createGroup(int owner, int cover, String name, String password, long createTime) {
-        // todo
-        return 0;
+        // 添加小组
+
+        GroupEntity groupEntity = new GroupEntity();
+        groupEntity.setCover(cover);
+        groupEntity.setOwner(owner);
+        groupEntity.setPassword(password);
+        groupEntity.setName(name);
+        groupEntity.setCreateTime(createTime);
+        boolean flag= groupDao.createGroup(sqlSession,groupEntity);
+        return flag ? groupEntity.getGid():0;
     }
 
     public GroupEntity getByGid(int gid) {
-        // todo
-        return null;
+        // 查找小组
+        return groupDao.getGroupByGid(sqlSession,gid);
     }
 }
